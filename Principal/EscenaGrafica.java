34072a4432f9dfe090d9b4fba084c8e27c7c383b
JuanRivas7 <jr6814353@gmail.com>
2025-04-13 18:39:49 -0600

    Ventana corrediza

--- Principal/EscenaGrafica.java
+++ Principal/EscenaGrafica.java
@@ -27,6 +27,7 @@ import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 import javax.vecmath.Vector3f;
 import steve.crearEscenaGrafica;
+import java.util.ArrayList;
 
 /**
  *
@@ -34,16 +35,16 @@ import steve.crearEscenaGrafica;
  */
 public class EscenaGrafica {
 
+    ArrayList<Ventana> listaVentanas = new ArrayList<>();
     BranchGroup objRaiz = new BranchGroup();
     Textura textura = new Textura();
     int paraTextura = Primitive.GENERATE_NORMALS + Primitive.GENERATE_TEXTURE_COORDS;
     color c = new color();
     TransformGroup tgMundo;
     TransformGroup tgPiso;
-    crearEscenaGrafica steve= new crearEscenaGrafica();
+    crearEscenaGrafica steve = new crearEscenaGrafica();
     Point3d posPersonaje = new Point3d(0, 0, 0);
     SerialPort puerto;
-    
     public EscenaGrafica() {
         //------- MUNDO--------
         Box bxMundo = new Box(-8.0f, 10.0f, 10.0f, paraTextura, textura.crearTexturas("cielo_1.jpg"));//c.setColor(38, 238, 240)
@@ -51,16 +52,17 @@ public class EscenaGrafica {
         t3dMundo.set(new Vector3d(0.0f, -0.15f, 0.0f));
         tgMundo = new TransformGroup(t3dMundo);
         //--------PISO-----------
-        Box bxPiso = new Box(2.0f, 0.05f, 2.0f, paraTextura, textura.crearTexturas("piso.jpg"));//c.setColor(38, 238, 240)
+        Box bxPiso = new Box(2.0f, 0.05f, 2.0f, paraTextura, textura.crearTexturas("piso_1.jpg"));//c.setColor(38, 238, 240)
         Transform3D t3dPiso = new Transform3D();
         t3dPiso.set(new Vector3d(0.0f, -0.28f, 0.0f));
         tgPiso = new TransformGroup(t3dPiso);
         EscalarTG(tgPiso, 5.0f);
         //-----------PAREDES Y VENTANAS------------
-         //crearParedCompleta(-0.2f, -0.1f, 0.0f, 0.15f, 0.2f, 0.05f, 255, 167, 38, -10);
+        //crearParedCompleta(-0.2f, -0.1f, 0.0f, 0.15f, 0.2f, 0.05f, 255, 167, 38, -10);
         //crearParedCompleta(-0.5f, -0.1f, 0.0f, 0.15f, 0.2f, 0.05f, 255, 167, 38, -10);
         //crearParedCompleta(-0.8f, -0.1f, 0.0f, 0.15f, 0.2f, 0.05f, 255, 167, 38, -10);
         //crearParedCompleta(-0.11f, -0.1f, 0.0f, 0.15f, 0.2f, 0.05f, 255, 167, 38, -10);
+        crearVentana(0.0f, 0.1f, 0.0f, 0.3f, 0.3f, 0.05f, 0);
         ///||------------PERMISOS------------------||
         tgMundo.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
         tgPiso.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
@@ -70,7 +72,6 @@ public class EscenaGrafica {
         myMouseRotate.setTransformGroup(tgMundo);
         myMouseRotate.setSchedulingBounds(new BoundingSphere());
 
-        
         //------------Termina pa que se mueva-------------
         //----------ADD CHILD----------
         tgMundo.addChild(bxMundo);
@@ -83,6 +84,7 @@ public class EscenaGrafica {
         objRaiz.addChild(steve.obtenerCuerpo());
         configurarIluminacion(objRaiz);
         conectarPuerto();
+
     }
 
     private void crearParedCompleta(float x, float y, float z,
@@ -110,13 +112,46 @@ public class EscenaGrafica {
         TransformGroup tgPared = new TransformGroup(t3dPared);
         objRaiz.addChild(tgPared);
         tgPared.addChild(bxPared);
+
     }
 // En tu clase EscenaGrafica
 
-    private void crearVentana(float x, float y, float z, float ancho, float alto,
-            float grosorMarco, float angulo) {
-        Ventana ventana = new Ventana(x, y, z, ancho, alto, grosorMarco, angulo);
-        objRaiz.addChild(ventana.getVentana());
+    public void crearVentana(float x, float y, float z, float ancho, float alto, float profundidad, float rotYGrados) {
+        Ventana ventana = new Ventana(x, y, z, ancho, alto, profundidad, rotYGrados);
+        objRaiz.addChild(ventana);
+        listaVentanas.add(ventana);
+    }
+
+    private void crearDetectorProximidad(final Ventana ventana, final float umbral) {
+        Behavior detector = new Behavior() {
+            WakeupOnElapsedTime tiempo = new WakeupOnElapsedTime(200);
+
+            public void initialize() {
+                wakeupOn(tiempo);
+            }
+
+            public void processStimulus(java.util.Enumeration criteria) {
+                Vector3f posVentana = ventana.getPosicion();
+
+                // Convertimos posición del personaje (Point3d) a Vector3f
+                Vector3f posPersonajeVec = new Vector3f((float) posPersonaje.x, (float) posPersonaje.y, (float) posPersonaje.z);
+
+                // Calculamos distancia
+                float dx = posVentana.x - posPersonajeVec.x;
+                float dy = posVentana.y - posPersonajeVec.y;
+                float dz = posVentana.z - posPersonajeVec.z;
+                float distancia = (float) Math.sqrt(dx * dx + dy * dy + dz * dz);
+
+                if (distancia < umbral) {
+                    ventana.toggle(); // Abre o cierra
+                }
+
+                wakeupOn(tiempo);
+            }
+        };
+
+        detector.setSchedulingBounds(new BoundingSphere(new Point3d(), 100.0));
+        objRaiz.addChild(detector);
     }
 
     public void EscalarTG(TransformGroup tg, float x) {
@@ -149,7 +184,8 @@ public class EscenaGrafica {
         luzRelleno.setInfluencingBounds(new BoundingSphere(new Point3d(0, 0, 0), 100.0));
         escena.addChild(luzRelleno);
     }
-    public void moverBehaivor(){
+
+    public void moverBehaivor() {
         KeyNavigatorBehavior keyNav = new KeyNavigatorBehavior(tgMundo);
         keyNav.setSchedulingBounds(new BoundingSphere(new Point3d(), 1000.0));
 
@@ -181,48 +217,50 @@ public class EscenaGrafica {
         objRaiz.addChild(animacionBehavior);
         tgMundo.addChild(keyNav);
     }
+
     private void rotarTG(TransformGroup tg, int grados, String eje, Point3d personajePosition) {
-    Transform3D transformacionMundo = new Transform3D();
-    tg.getTransform(transformacionMundo);
-    
-    // Crear transformacion para mover el mundo desde el personaje
-    Transform3D traslacionAlOrigen = new Transform3D();
-    traslacionAlOrigen.setTranslation(new Vector3d(
-        -personajePosition.x,
-        -personajePosition.y,
-        -personajePosition.z
-    ));
-    
-    Transform3D rotacion = new Transform3D();
-    switch (eje) {
-        case "X":
-            rotacion.rotX(Math.PI / 180 * grados);
-            break;
-        case "Y":
-            rotacion.rotY(Math.PI / 180 * grados);
-            break;
-        case "Z":
-            rotacion.rotZ(Math.PI / 180 * grados);
-            break;
-        default:
-            break;
+        Transform3D transformacionMundo = new Transform3D();
+        tg.getTransform(transformacionMundo);
+
+        // Crear transformacion para mover el mundo desde el personaje
+        Transform3D traslacionAlOrigen = new Transform3D();
+        traslacionAlOrigen.setTranslation(new Vector3d(
+                -personajePosition.x,
+                -personajePosition.y,
+                -personajePosition.z
+        ));
+
+        Transform3D rotacion = new Transform3D();
+        switch (eje) {
+            case "X":
+                rotacion.rotX(Math.PI / 180 * grados);
+                break;
+            case "Y":
+                rotacion.rotY(Math.PI / 180 * grados);
+                break;
+            case "Z":
+                rotacion.rotZ(Math.PI / 180 * grados);
+                break;
+            default:
+                break;
+        }
+
+        // Posición original
+        Transform3D traslacionDeVuelta = new Transform3D();
+        traslacionDeVuelta.setTranslation(new Vector3d(
+                personajePosition.x,
+                personajePosition.y,
+                personajePosition.z
+        ));
+
+        transformacionMundo.mul(traslacionDeVuelta, transformacionMundo);
+        transformacionMundo.mul(rotacion, transformacionMundo);
+        transformacionMundo.mul(traslacionAlOrigen, transformacionMundo);
+
+        tg.setTransform(transformacionMundo);
     }
-    
-    // Posición original
-    Transform3D traslacionDeVuelta = new Transform3D();
-    traslacionDeVuelta.setTranslation(new Vector3d(
-        personajePosition.x,
-        personajePosition.y,
-        personajePosition.z
-    ));
-    
-    transformacionMundo.mul(traslacionDeVuelta, transformacionMundo);
-    transformacionMundo.mul(rotacion, transformacionMundo);
-    transformacionMundo.mul(traslacionAlOrigen, transformacionMundo);
-    
-    tg.setTransform(transformacionMundo);
-}
- private void conectarPuerto() {
+
+    private void conectarPuerto() {
         puerto = SerialPort.getCommPort("COM5");
         puerto.setBaudRate(9600);
         puerto.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
@@ -247,7 +285,9 @@ public class EscenaGrafica {
                             String linea = mensaje.toString().trim();
                             mensaje.setLength(0); // Limpia el buffer
                             System.out.println("Recibido: " + linea);
-
+                            if (linea.equals("Presionado  Boton")) {
+                                verificarVentanasCercanasYTogglear();
+                            }
                             if (linea.equals("Suelto  Arriba") || linea.equals("Presionado  Arriba")) {
                                 MoverAdelante(tgMundo, steve.obtenerPanza(), 0.15);
                                 steve.caminar();
@@ -276,7 +316,8 @@ public class EscenaGrafica {
             System.out.println("No se pudo abrir el puerto");
         }
     }
-   private void MoverAdelante(TransformGroup tgMundo, TransformGroup tgPersonaje, double velocidad) {
+
+    private void MoverAdelante(TransformGroup tgMundo, TransformGroup tgPersonaje, double velocidad) {
         Transform3D t3dPersonaje = new Transform3D();
         tgPersonaje.getTransform(t3dPersonaje);
 
@@ -302,7 +343,7 @@ public class EscenaGrafica {
         t3dMundo.setTranslation(posicionMundo);
         tgMundo.setTransform(t3dMundo);
     }
-   
+
     private void MoverAtras(TransformGroup tgMundo, TransformGroup tgPersonaje, double velocidad) {
         Transform3D t3dPersonaje = new Transform3D();
         tgPersonaje.getTransform(t3dPersonaje);
@@ -329,4 +370,22 @@ public class EscenaGrafica {
         t3dMundo.setTranslation(posicionMundo);
         tgMundo.setTransform(t3dMundo);
     }
+    private void verificarVentanasCercanasYTogglear() {
+    Vector3f posPersonajeVec = new Vector3f((float) posPersonaje.x, (float) posPersonaje.y, (float) posPersonaje.z);
+
+    for (Ventana ventana : listaVentanas) {
+        Vector3f posVentana = ventana.getPosicion();
+
+        float dx = posVentana.x - posPersonajeVec.x;
+        float dy = posVentana.y - posPersonajeVec.y;
+        float dz = posVentana.z - posPersonajeVec.z;
+        float distancia = (float)Math.sqrt(dx*dx + dy*dy + dz*dz);
+
+        if (distancia < 0.3f) { // Puedes ajustar el umbral
+            ventana.toggle(); // Abre o cierra
+            break; // Solo una ventana por clic
+        }
+    }
+}
+
 }
